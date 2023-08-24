da95067bbd8ad665169f18d9eb6642d0bd2638d5
Yahya <yahyahussein670@gmail.com>
2023-08-24 11:35:10 +0300

    ✨ feat: generate ConversationalLearningPage Component

--- libs/features/pages/conversational-learning/src/lib/features-pages-conversational-learning.module.ts
+++ libs/features/pages/conversational-learning/src/lib/features-pages-conversational-learning.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConversationalLearningPageComponent } from './main/conversational-learning-page/conversational-learning-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ConversationalLearningPageComponent],
 })
 export class FeaturesPagesConversationalLearningModule {}
